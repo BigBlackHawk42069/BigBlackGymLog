7c1c16b4bbd63a0fa92ac84744b3b5a8fcc93588
eJunk Studios <firstnamedotlastname42@gmail.com>
2026-05-27 02:56:50 -0500

    Updates

--- src/00-header.js
+++ src/00-header.js
@@ -7,6 +7,6 @@
 // @match        https://www.torn.com/*
 // @grant        none
 // @run-at       document-start
-// @updateURL    https://raw.githubusercontent.com/BigBlackHawk42069/Big-Black-Gym-Log/refs/heads/main/BBGL%20Test%20Branch/Big-Black-Gym-Log-Test.js
-// @downloadURL  https://raw.githubusercontent.com/BigBlackHawk42069/Big-Black-Gym-Log/refs/heads/main/BBGL%20Test%20Branch/Big-Black-Gym-Log-Test.js
+// @updateURL    https://raw.githubusercontent.com/BigBlackHawk42069/BBGLTeste/refs/heads/main/Big-Black-Gym-Log-Test.js
+// @downloadURL  https://raw.githubusercontent.com/BigBlackHawk42069/BBGLTeste/refs/heads/main/Big-Black-Gym-Log-Test.js
 // ==/UserScript==
